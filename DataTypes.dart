745f4d87afa0b508bd0837b94e0def4320c99650
Ujwal-Khairnar <ujwalkhairnar5@gmail.com>
2023-09-26 15:26:18 +0530

    DataTypes and variables

---
+++ DataTypes.dart
@@ -0,0 +1,27 @@
+void main()
+{
+    //we want dont want store anything in the variable then we can use ( ? ) symbol to make it null
+    int? a;
+    print(a);
+
+    //Integer
+    int a=5;
+    print(a);
+
+    //Double
+    double b=5.3;
+    print(b);
+
+    //String
+    String c="Ujwal Khairnar";
+    print(c);
+
+    //Big Int
+    BigInt d;
+    d=BigInt.parse('534324343243243423');
+    print(d);
+
+    bool val=true;
+    print(val);
+  
+}
